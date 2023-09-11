49c50bcf8db174693708b16709e4ff57cffe1ba0
Crowdbotics <team@crowdbotics.com>
2023-09-11 10:26:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-disk-43326",
+  url: "https://falling-disk-43326.botics.co",
+  api: "https://falling-disk-43326.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
